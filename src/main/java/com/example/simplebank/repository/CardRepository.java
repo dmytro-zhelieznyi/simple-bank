de69260dc4a2c489622750ae79aec1b3f013fb86
dmytro-zhelieznyi <48699067+dmytro-zhelieznyi@users.noreply.github.com>
2022-10-05 20:17:22 +0200

    card-generation-validator (#4)
    created validation to check if new card number is unique
    
    Co-authored-by: Dmytro Zhelieznyi <Dmytro.Zhelieznyi@amway.com>

--- src/main/java/com/example/simplebank/repository/CardRepository.java
+++ src/main/java/com/example/simplebank/repository/CardRepository.java
@@ -3,6 +3,10 @@ package com.example.simplebank.repository;
 import com.example.simplebank.data.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    Optional<Card> findByNumber(String number);
 
 }
