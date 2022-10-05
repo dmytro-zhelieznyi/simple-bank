11a29eecc82c5f76449256272280a02ec99ab3e7
Dmytro Zhelieznyi <Dmytro.Zhelieznyi@amway.com>
2022-10-05 18:35:46 +0200

    [feat] * created cards mock data

--- src/main/java/com/example/simplebank/service/CardService.java
+++ src/main/java/com/example/simplebank/service/CardService.java
@@ -5,14 +5,32 @@ import com.example.simplebank.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class CardService {
 
     private final CardRepository cardRepository;
+
+    @PostConstruct
+    public void postConstructor() {
+        // TODO change with liquibase change sets
+        List<Card> cards = Stream.generate(this::createCard)
+                .limit(10)
+                .toList();
+
+        cards.forEach(card -> {
+            card.setAmount(new BigDecimal(10_000));
+            updateCard(card);
+        });
+    }
 
     public Card createCard() {
         Card card = Card.builder()
@@ -24,6 +42,10 @@ public class CardService {
         return cardRepository.save(card);
     }
 
+    public Card updateCard(Card card) {
+        return cardRepository.save(card);
+    }
+
     private String generateUniqueCardNumber() {
         Random random = new Random();
         StringBuilder number = new StringBuilder();
@@ -31,7 +53,7 @@ public class CardService {
             number.append(random.nextInt(9) + 1);
         }
 
-        // TODO validator to check if number is unique
+        // TODO create validator to check if number is unique
         return number.toString();
     }
 
